c94c1e65097e7b264b4a5466170ef6f9d0c7658b
NASCC <pedro.main.tec@gmail.com>
2024-09-20 19:31:16 -0300

    [UPDATED] Disable relative numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable relative numbers (i don't like it a lot :^(  )
+vim.opt.relativenumber = false
