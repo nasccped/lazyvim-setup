e26323e96608597909d27740fa951eccd2ead63c
NASCC <pedro.main.tec@gmail.com>
2024-09-26 20:53:16 -0300

    [UPDATED] Setting  as main theme

--- lua/plugins/theme-nordic.lua
+++ lua/plugins/theme-nordic.lua
@@ -1,6 +1,20 @@
 return {
 
-  { "AlexvZyl/nordic.nvim" },
+  {
+    "AlexvZyl/nordic.nvim",
+
+    -- configs to set as main theme
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme nordic")
+    end,
+  },
 
   { "LazyVim/LazyVim" },
 }
